e2d54e2606691a45858de18acbb980325ec594b8
collins ochieng <collotuos0@gmail.com>
2019-06-23 23:52:40 +0300

    add username and online user functionality

--- server.js
+++ server.js
@@ -10,21 +10,37 @@ server.listen(process.env.PORT || 3000);
 
 console.log("Server running.....");
 
+// using express to serve the html file for browswers
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
 
-io.sockets.on('connection', (socket)=>{
+io.sockets.on('connect', (socket)=>{
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length)
     
 socket.on('disconnect', (data)=>{
     connections.splice(connections.indexOf(socket),1);
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames()
+    console.log(users)
     console.log('Disconnected: %s sockets connected', connections.length)
 })
 socket.on('send message', function(data){
     console.log(data)
-    io.sockets.emit('new message', {msg:data})
+    io.sockets.emit('new message', {msg:data, username:socket.username})
 });
+
+socket.on('new user', (username, callback)=>{
+    callback(username)
+    socket.username = username;
+    users.push(username)
+    console.log(users)
+    updateUsernames()
+});
+// ES6 function definition
+const updateUsernames = ()=>{
+    io.sockets.emit('get users', users)
+}
 
 })
